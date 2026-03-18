beb243cf45a579b939b2dc2b1b8f2dd5fecf8c8a
JH <rodan1618@gmail.com>
2026-03-18 11:08:37 -0300

    segunda alteração

---
+++ funcao.py
@@ -0,0 +1,14 @@
+### Definição de função:
+#Função são blocos de códicos nomeados capazes de realizar uma ação específica, se quisermos excutar essa ação específica, é só "Chamar" excrevendo o nome da função.
+### SINTAXE: 
+# def nome_da_funcao():
+    #código
+#nome_da_funcao
+
+def saudacao(nome):
+    print("Ola,", nome, "seja bem-vindo")
+saudacao("Rodan")
+saudacao("Lucas")
+saudacao("José") 
+#com um único print podemos colocar vários nome
+#ou 2° opção abaixo:
